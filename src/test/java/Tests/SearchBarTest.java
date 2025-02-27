345b2c99db51d881e8e46b55bbf3e2fd6ae3b09d
asus <zmileshin@gmail.com>
2025-02-26 23:49:17 -0500

    input search bar test

--- src/test/java/Tests/SearchBarTest.java
+++ src/test/java/Tests/SearchBarTest.java
@@ -1,8 +1,10 @@
 package Tests;
 
 import Pages.HomePage;
-import Utility.ConfigReader;
 import Utility.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 public class SearchBarTest extends TestBase{
 
@@ -14,5 +16,13 @@ public class SearchBarTest extends TestBase{
 
         searchBar.getSearchField().sendKeys("test");
         System.out.println("OK");
+    }
+    
+    @Test
+    public void characterInput(){
+        HomePage input = new HomePage();
+        String key ="####" ;
+        input.getSearchField().sendKeys(key, Keys.ENTER);
+        Assert.assertTrue(Driver.getDriver().findElement(By.xpath("//div[@class='padded']")).getText().contains("Sorry, we couldn't find any matches for "+"\""+key+"\"" ));
     }
 }
